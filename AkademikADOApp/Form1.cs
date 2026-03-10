a8e5457445bd544cae14709228dcd9b2d7d1caff
keishashendy <shendyarrafli7@gmail.com>
2026-03-10 12:32:20 +0700

    Menambahkan status berhasil saat connect database

--- AkademikADOApp/Form1.cs
+++ AkademikADOApp/Form1.cs
@@ -27,3 +27,11 @@ namespace AkademikADOApp
             {
                 conn = new SqlConnection(connString);
                 conn.Open();
+                lblStatus.Text = "Status : Database Connected";
+                MessageBox.Show("Koneksi ke database berhasil!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Koneksi gagal: " + ex.Message);
+            }
+        }
