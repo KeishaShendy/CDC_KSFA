d4c672b5b9890c0f5b892fb5b01edc7630476b9b
keishashendy <shendyarrafli7@gmail.com>
2026-03-10 12:26:59 +0700

    Menambahkan namespace yang dibutuhkan

--- AkademikADOApp/Form1.cs
+++ AkademikADOApp/Form1.cs
@@ -3,15 +3,3 @@ using System;
 using System.Data;
 using System.Data.SqlClient;
 using System.Windows.Forms;
-
-namespace AkademikADOApp
-{
-    public partial class Form1 : Form
-    {
-        string connString = @"Data Source=LAPTOP-MBD0B33T\SHENDY;Initial Catalog=DBAkademikADO;Integrated Security=True";
-        SqlConnection conn;
-
-        public Form1()
-        {
-            InitializeComponent();
-        }
